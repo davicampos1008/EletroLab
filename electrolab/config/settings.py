158dd2c05676ad84295def6f5348de8b544fb7e0
Davi Campos <nygames92@gmail.com>
2026-09-03 10:06:13 -0300

    asdac

--- electrolab/config/settings.py
+++ electrolab/config/settings.py
@@ -7,8 +7,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = 'django-insecure-vkcx1)tkli8(6+zq3jb7=6=nirfq7*qjv562qhfz04et4+warx'
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = True 
+
+# Configuração de segurança básica para o Render e Local
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -23,6 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # <-- A MÁGICA ACONTECE AQUI (Obrigatório pro Render)
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -64,19 +67,17 @@ AUTH_PASSWORD_VALIDATORS = [
     { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator' },
 ]
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+# Idioma e Fuso Horário do Brasil
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
 # Configuração de arquivos estáticos com WhiteNoise para produção
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # <-- Barra inicial obrigatória adicionada
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-# Configuração de segurança básica para o Render
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
